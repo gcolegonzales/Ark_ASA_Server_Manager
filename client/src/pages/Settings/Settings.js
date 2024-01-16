b1c558df8f8f814e7d1933012a87fb441e2834a8
Cole Gonzales <gcolegonzales003@gmail.com>
2024-01-16 01:43:32 -0500

    Also adding a display for servers main directory to make the Settings page look more real

--- client/src/pages/Settings/Settings.js
+++ client/src/pages/Settings/Settings.js
@@ -5,6 +5,7 @@ export const Settings = ({ server }) => {
     <div>
       <h1>Settings</h1>
       <h5>Server: {server.name}</h5>
+      <h5>Main Directory: {server.mainDirectory}</h5>
     </div>
   );
 }
